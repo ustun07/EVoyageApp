d98d840ff3ae979e5852d0e95359bec48af8bc52
Selim Can Üstün <scanustun@hotmail.com>
2024-04-21 17:58:07 +0300

    Initial Commit

---
+++ BitirmeProje/BitirmeProjeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitirmeProjeApp.swift
+//  BitirmeProje
+//
+//  Created by Selim Can Üstün on 21.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct BitirmeProjeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
